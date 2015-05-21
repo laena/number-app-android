3b332ba1dccd17104a34d95714144925f0c66f6c
Lena <lenaherscheid@gmail.com>
2015-05-21 16:22:46 +0200

    added normal random answer generation

--- assets/www/util.js
+++ assets/www/util.js
@@ -1,3 +1,16 @@
+function normalRandom(mean, deviation) {
+	// https://en.wikipedia.org/wiki/Marsaglia_polar_method
+    var s, u, v, n;
+    do {
+        u = Math.random() * 2 - 1;
+        v = Math.random() * 2 - 1;
+        s = u * u + v * v;
+    } while (s >= 1);
+    
+    n = u * Math.sqrt(-2 * Math.log(s) / s);
+    return Math.floor(deviation * n + mean);
+}
+
 function randomRange(min, max) {
     return Math.floor((Math.random() * (max - min)) + min);
 }
